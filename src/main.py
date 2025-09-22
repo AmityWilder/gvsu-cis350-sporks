6add87e7f4b1b4ed54bc2a191df7bdbf2fc3ad39
Amy Wilder <74995093+HenryWilder@users.noreply.github.com>
2025-09-22 16:33:23 -0400

    Add comments

--- src/main.py
+++ src/main.py
@@ -2,14 +2,13 @@ import sys
 from subprocess import Popen, PIPE
 from threading import Thread
 
-SERVER_EXE_PATH = "./target/debug/gvsu-cis350-sporks" # CHANGE THIS
+# Make sure this is the location of the server file on the end user's device
+SERVER_EXE_PATH = "./target/debug/gvsu-cis350-sporks"
 
-with Popen([SERVER_EXE_PATH], stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True) as proc:
+with Popen([SERVER_EXE_PATH], stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True) as process:
     print("opening server")
-    while proc.poll() is None:
-        outs, errs = proc.communicate("--exit")
-        for out in outs:
-            print("out: ", out)
-        for err in errs:
-            print("err: ", errs)
+    while process.poll() is None: # while the process is running
+        outputs, errors = process.communicate("--exit") # write "--exit" to stdin
+        print("out: ", outputs)
+        print("err: ", errors)
     print("process closed")
