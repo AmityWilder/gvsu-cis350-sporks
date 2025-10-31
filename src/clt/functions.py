77c205df0a30a5731b741a3d4c3d11f3e5d78759
Conor <buchkowc@mail.gvsu.edu>
2025-10-31 13:16:07 -0400

    made a manager button that toggles add user and add task buttons

--- src/clt/functions.py
+++ src/clt/functions.py
@@ -8,12 +8,16 @@ def add_task(proxy):
 def add_user(proxy):
     added = proxy.add_users({'to_add': [{'name': "Edward Coolguy"}]})
 
-def toggle_textbox(textbox_visible,entry_box,toggle_button):
-    if textbox_visible.get():  # If textbox is currently visible
-        entry_box.pack_forget()  # Hide the textbox
-        textbox_visible.set(False)
-        toggle_button.config(text="Show Textbox")
+def toggle_element(element_visible,element):
+    if element_visible.get():  # If textbox is currently visible
+        for i in range(len(element)):
+            current=element[i]
+            current.pack_forget()  # Hide the textbox
+        element_visible.set(False)
+        #toggle_button.config(text="Show Textbox")
     else:  # If textbox is currently hidden
-        entry_box.pack()  # Show the textbox
-        textbox_visible.set(True)
-        toggle_button.config(text="Hide Textbox")
+        for j in range(len(element)):
+            current=element[j]
+            current.pack()  # Show the textbox
+        element_visible.set(True)
+        #toggle_button.config(text="Hide Textbox")
