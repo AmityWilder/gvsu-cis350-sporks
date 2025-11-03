942acc6dee7e7c0ada8febc170611fe994c84c51
Conor <buchkowc@mail.gvsu.edu>
2025-11-03 13:57:05 -0500

    made cancel button work

--- src/clt/functions.py
+++ src/clt/functions.py
@@ -1,8 +1,18 @@
 import time, xmlrpc.client, subprocess, atexit, tkinter as tk
 
-def on_button_click(label):
-    """Function to be called when the button is clicked."""
-    label.config(text="Button was clicked!")
+def cancel(curr_element_visible,opp_element_visible, element, opp_element, Cancel):
+    if curr_element_visible.get():  # If textbox is currently visible
+        for i in range(len(element)):
+            current=element[i]
+            current.pack_forget()  # Hide the textbox
+        curr_element_visible.set(False)
+        Cancel.pack_forget()
+    if opp_element_visible.get():
+        for n in range(len(opp_element)):
+            current=opp_element[n]
+            current.pack_forget()  # Hide the textbox
+        opp_element_visible.set(False)
+        Cancel.pack_forget()
 
 def add_task(proxy):
     added = proxy.add_tasks({'to_add': [{'title': "foo"}]})
