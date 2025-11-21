fc00f2ded851d8f7646dc5afb70ca28f9515ded2
Amy Wilder <74995093+HenryWilder@users.noreply.github.com>
2025-11-21 15:56:42 -0500

    Create stub

---
+++ src/clt/integration-tests.py
@@ -0,0 +1,39 @@
+import time, xmlrpc.client, subprocess, atexit, tkinter as tk
+
+IS_DEBUG_BUILD = True
+if IS_DEBUG_BUILD:
+    BUILD = "debug"
+else:
+    BUILD = "release"
+
+# open the server in parallel
+srv = subprocess.Popen([f"./target/{BUILD}/gvsu-cis350-sporks"])
+
+# create a line of communication with the server
+with xmlrpc.client.ServerProxy("http://127.0.0.1:8080") as proxy:
+    def close_server():
+        print("attempting to close server")
+        proxy.quit({})
+        slept = 0
+        while srv.poll() is None:
+            # still running after 5 seconds
+            if slept >= 2:
+                print("close failed, terminating server")
+                srv.terminate()
+                break
+            else:
+                time.sleep(0.01)
+                slept += 0.01
+        slept = 0
+        while srv.poll() is None:
+            # still running 5 seconds after termination
+            if slept >= 5:
+                print("termination failed, killing server")
+                srv.kill()
+                break
+            else:
+                time.sleep(0.01)
+                slept += 0.01
+        print("finished")
+
+    atexit.register(close_server)
