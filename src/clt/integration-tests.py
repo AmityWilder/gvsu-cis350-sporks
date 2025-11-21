dbc36d3c1c80f5f03f69bb5765069728d1d53150
Amy Wilder <74995093+HenryWilder@users.noreply.github.com>
2025-11-21 17:06:14 -0500

    Add preliminary tests

--- src/clt/integration-tests.py
+++ src/clt/integration-tests.py
@@ -1,4 +1,40 @@
-import time, xmlrpc.client, subprocess, atexit, tkinter as tk
+import time, xmlrpc.client, subprocess, atexit, datetime
+
+
+def test_add_one_user(proxy):
+    user = {
+        'name': "Squeeby Deeby",
+    }
+    ids = proxy.add_users({'to_add': [user]})
+    assert ids is not None and len(ids) == 1
+
+
+def test_add_multiple_users(proxy):
+    user1 = {
+        'name': "Dill Pickle",
+    }
+    user2 = {
+        'name': "Mark Hapenstance",
+    }
+    ids = proxy.add_users({'to_add': [user1, user2]})
+    assert ids is not None and len(ids) == 2
+
+
+def test_add_one_slot(proxy):
+    proxy.add_slots([{
+        'start': datetime.datetime(2006, 11, 21, 16, 30),
+        'end': datetime.datetime(2006, 11, 21, 16, 45),
+        'min_staff': 3,
+        'name': "Sweep dishes",
+    }])
+
+
+TESTS = [
+    ('test_add_one_slot', test_add_one_slot),
+    ('test_add_one_user', test_add_one_user),
+    ('test_add_multiple_users', test_add_multiple_users),
+]
+
 
 IS_DEBUG_BUILD = True
 if IS_DEBUG_BUILD:
@@ -8,12 +44,19 @@ else:
 
 # open the server in parallel
 srv = subprocess.Popen([f"./target/{BUILD}/gvsu-cis350-sporks"])
+time.sleep(0.01) # wait for the server to be open
 
 # create a line of communication with the server
-with xmlrpc.client.ServerProxy("http://127.0.0.1:8080") as proxy:
+with xmlrpc.client.ServerProxy("http://127.0.0.1:8080", use_datetime=True) as proxy:
+
     def close_server():
         print("attempting to close server")
-        proxy.quit({})
+        if srv.poll() is not None:
+            return # server has already closed
+        try:
+            proxy.quit({})
+        except:
+            pass # ignore failure, we need to kill it if it won't go quietly
         slept = 0
         while srv.poll() is None:
             # still running after 5 seconds
@@ -37,3 +80,11 @@ with xmlrpc.client.ServerProxy("http://127.0.0.1:8080") as proxy:
         print("finished")
 
     atexit.register(close_server)
+
+    for name,f in TESTS:
+        print(name, end=': ')
+        try:
+            f(proxy)
+            print("passed")
+        except Exception as e:
+            print(f"failed\n  {e}")
