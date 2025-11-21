0e09ad7944423055e5531a56f4cb1969691d617d
Amy Wilder <74995093+HenryWilder@users.noreply.github.com>
2025-11-21 15:52:51 -0500

    Add server downloader script

---
+++ update-server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+case $1 in
+    '-r') build='release';;
+    *|'-d') build='debug';;
+esac
+
+if command -v cargo
+then
+    echo "Building with \`cargo\`..."
+    if [ "$build" = 'release' ]; then
+        is_release=1
+    fi
+    cargo build ${is_release:+'--release'}
+else
+    file='gvsu-cis350-sporks.exe'
+    download="https://github.com/AmityWilder/gvsu-cis350-sporks/releases/latest/download/$file"
+    dest="$(dirname "$0")/target/$build/$file"
+    echo "Downloading \`$download\` into \`$dest\`..."
+    curl -L $download --create-dirs -o $dest
+fi
