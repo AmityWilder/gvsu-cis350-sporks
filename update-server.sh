3bbcbed26524b5f0ed6a391473ef11b925955417
Amy Wilder <74995093+HenryWilder@users.noreply.github.com>
2025-11-21 15:56:21 -0500

    Change script to use curl as fallback for failed builds

--- update-server.sh
+++ update-server.sh
@@ -10,11 +10,15 @@ then
     if [ "$build" = 'release' ]; then
         is_release=1
     fi
-    cargo build ${is_release:+'--release'}
-else
-    file='gvsu-cis350-sporks.exe'
-    download="https://github.com/AmityWilder/gvsu-cis350-sporks/releases/latest/download/$file"
-    dest="$(dirname "$0")/target/$build/$file"
-    echo "Downloading \`$download\` into \`$dest\`..."
-    curl -L $download --create-dirs -o $dest
+    if cargo build ${is_release:+'--release'}; then
+        exit 0
+    else
+        echo 'Build failed.'
+    fi
 fi
+
+file='gvsu-cis350-sporks.exe'
+download="https://github.com/AmityWilder/gvsu-cis350-sporks/releases/latest/download/$file"
+dest="$(dirname "$0")/target/$build/$file"
+echo "Downloading \`$download\` into \`$dest\`..."
+curl -L $download --create-dirs -o $dest
